56898463ea2db2d88a7df1ba18c7bf2f001a421f
Lars Robeerst <larsrobeerst1@gmail.com>
2022-12-22 17:25:00 +0100

    keuken type class aangemaakt (#7)

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 require_once("lib/database.php");
 require_once("lib/artikel.php");
 require_once("lib/user.php");
-
+require_once("lib/keuken_type.php");
 /// INIT
 $db = new database();
 $art = new artikel($db->getConnection());
 $usr = new user($db->getConnection());
+$kte = new keuken_type($db->getConnection());
 
 
 /// VERWERK 
-$testData = $usr->selecteerUser(1);
+$testData = $kte->selecteerKeukenType(3);
 
 
 
---
+++ lib/keuken_type.php
@@ -0,0 +1,23 @@
+<?php
+
+class keuken_type {
+
+    private $connection;                        ///always private
+
+    public function __construct($connection) {  ///always public
+        $this->connection = $connection;
+    }
+  
+    public function selecteerKeukenType($keuken_type_id) { /// depends on the class wether public or private (left or right asd)
+        $sql = "select * from keuken_type where id = $keuken_type_id";
+        
+    
+        $result = mysqli_query($this->connection, $sql);
+        $keuken_type = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        return($keuken_type);
+
+    }
+
+
+}
